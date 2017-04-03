e3b4df26f075620f731ec8d5f6bc7812c72f82a4
admin <luciano101@msn.com>
2017-04-03 18:22:03 -0400

    Module Design Pattern

---
+++ moduleDesign/README.md
@@ -0,0 +1,10 @@
+# javaScriptPatterns
+A collection of resources on JS patterns
+
+##Module Design Patern
+
+Allows you to create:
+
+-Real Privacy Control
+-Have a clear public API
+-True encapsulation
--- oneObjNamespace/script.js
+++ oneObjNamespace/script.js
@@ -1,5 +1,22 @@
-// Namespace / Object Literal Pattern
+// Namespace 
+// var com  = com || {};
+// com.o2geek = com.o2geek || {};
+// com.o2geek.packt = com.o2geek.packt || {};
+// com.o2geek.packt.sudoChat  = {
+// 	leadself: 'Me: ',
+// 	leadcomputer: "PC: ",
+// 	aSaid: ["This is a Cyber Chat"],
+// 	msgYes:  "Yes, that's a great idea.",
+// 	msgNo: "No, that must be a mistake.",
+// 	aSassyStuff: ["Like mold on books, grow myths on history.",
+// 								"She moved like a poem and smiled like a sphinx.",
+// 								"As long as we don’t die, this is gonna be one hell of a story.",
+// 								"She laughed, and the desert sang.",
+// 								"You’ve got about as much charm as a dead slug."],
 
+
+
+// / Object Literal Pattern
 var o  = {
 	leadself: 'Me: ',
 	leadcomputer: "PC: ",
